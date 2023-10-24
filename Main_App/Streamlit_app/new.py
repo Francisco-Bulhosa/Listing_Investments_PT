a51964a2306625bca1b4245c1cf67ff834732d4a
FBF <francisco.bulhosa@gmail.com>
2023-10-24 22:57:03 +0100

    Update new.py

--- Main_App/Streamlit_app/new.py
+++ Main_App/Streamlit_app/new.py
@@ -34,49 +34,58 @@ from Paths.connect_db import create_connection
 listings_df = pd.DataFrame()
 
 
-conn = create_connection()
-if conn is None:
-    st.error("Unable to establish a connection to the database.")
-else:
-    listings_df = pd.read_sql_query("SELECT * FROM listings", conn)
+# Connect to the SQLite database for listings
+conn = sqlite3.connect(LISTINGS_DB_PATH)
+listings_df = pd.read_sql_query("SELECT * FROM listings", conn)
 
 
-# User Input
+# Load infrastructure data
+infra_df = pd.read_csv(INFRASTRUCTURE_CSV_PATH)
+
+# Title of the app
+st.title("Real Estate Investment Advisor")
+
+# 1. User Input
 investment_amount = st.number_input("How much do you intend to invest?", min_value=0.0, step=100.0)
-
-zones = ["Zone 1", "Zone 2", "Zone 3", "Any"]
+zones = ["Costa da Caparica", "Caparica e Trafaria", "Charneca de Caparica e Sobreda", "Laranjeiro e Feijó",
+"Almada, Cova da Piedade, Pragal e Cacilhas", "Any"]  # Replace your zones
 chosen_zone = st.selectbox("Select a zone:", zones)
 
 intentions = ["Build & Sell", "Rent", "Any"]
 chosen_intention = st.selectbox("What is your intention?", intentions)
 
-infra_options = ["Infrastructure 1", "Infrastructure 2", "Infrastructure 3", "Any"]
+infra_options = ["High Relevance (Bridge, Fórum)", "Hospital", "Metro & Train Station", "Beaches", "University", "Any"]
 chosen_infra = st.selectbox("Which infrastructure is most important to you?", infra_options)
 
 
-# Filter Listings
+# Filter Listings Based on User Input
 filtered_listings = listings_df
 if chosen_zone != "Any":
-    filtered_listings = filtered_listings[filtered_listings['zone'] == chosen_zone]
+    filtered_listings = filtered_listings[filtered_listings.zone == chosen_zone]
 
-# Initialize to an empty DataFrame
-sorted_listings = pd.DataFrame()  
-
-infra_options = ["Infrastructure 1", "Infrastructure 2", "Infrastructure 3", "Any"]
-
-# Apply scoring logic
+# 3. Apply Weights and Penalties
+# Note: The function updated_scoring_logic should take care of calculating scores, applying weights and penalties
 try:
     sorted_listings = updated_scoring_logic(filtered_listings, chosen_zone, chosen_infra, chosen_intention, investment_amount)
 except Exception as e:
-    st.error(f"An error occurred while applying scoring logic: {e}")
+    st.error(f"An error occurred while applying scoring logic: {str(e)}")
     sorted_listings = pd.DataFrame()
 
-
-
+# 4. Show Results
 if not sorted_listings.empty:
-    st.write(sorted_listings)
+    st.write("Top Results:")
+    st.write(sorted_listings.head(10))  # Displaying top 10 results
 else:
     st.write("No listings to display.")
 
+
+st.write("Investment Amount:", investment_amount)
+st.write("Chosen Zone:", chosen_zone)
+st.write("Chosen Intention:", chosen_intention)
+st.write("Chosen Infrastructure:", chosen_infra)
+
+
+st.write("Filtered Listings:")
+st.write(filtered_listings)
 
 
